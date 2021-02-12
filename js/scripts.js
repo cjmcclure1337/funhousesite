b17c487798a07668fe990978f15169b8e973c421
Chris <cjmcclure1337@gmail.com>
2021-02-12 15:51:18 -0600

    Shop pics and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
 $(function() {
-    $(".carousel").carousel( {interval:4000});
+    $(".carousel").carousel( {interval:4000, cycle=true}); 
 });
+
+
+var currentGlaze = "ebony";
+var currentItem = "mug";
+
+const radioEl = document.querySelector("div[id=glazeRadio]");
+radioEl.onclick = function() {
+    const glazes = document.querySelectorAll("input[name=glazeType]");
+    var selectedValue;
+    for (const rb of glazes) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            break;
+        }
+    }
+    
+    if (currentGlaze !== selectedValue){
+        currentGlaze = selectedValue;
+        document.getElementById("shopImg").src = "img/shopItems/" + currentItem + currentGlaze + ".webp";
+    }
+}
+
+const selectionEl = document.querySelector("select[id=potType]");
+selectionEl.onclick = function() {
+    if(selectionEl.value !== currentItem) {
+        currentItem = selectionEl.value;
+        document.getElementById("shopImg").src = "img/shopItems/" + currentItem + currentGlaze + ".webp";
+    }
+}
