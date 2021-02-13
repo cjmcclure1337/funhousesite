4e7cd8c3bf5e57d99b4a87882c81887ea83d507a
Chris <cjmcclure1337@gmail.com>
2021-02-12 18:06:31 -0600

    Misc Fixes and Alignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(function() {
-    $(".carousel").carousel( {interval:4000, cycle=true}); 
+    $(".carousel").carousel( {interval:4000}); 
+    $("#aboutCarouselPrev").click(function(){
+        $(".carousel").carousel("prev");
+    })
+    $("#aboutCarouselNext").click(function(){
+        $(".carousel").carousel("next");
+    })
 });
 
 
 var currentGlaze = "ebony";
 var currentItem = "mug";
 
+//On the Shop page, listens for a change to the radio (glaze selector) and updates the picture if needed
 const radioEl = document.querySelector("div[id=glazeRadio]");
 radioEl.onclick = function() {
     const glazes = document.querySelectorAll("input[name=glazeType]");
@@ -23,6 +30,7 @@ radioEl.onclick = function() {
     }
 }
 
+//On the Shop page, listens for a change to the select object (pot type selector) and updates the picture if needed
 const selectionEl = document.querySelector("select[id=potType]");
 selectionEl.onclick = function() {
     if(selectionEl.value !== currentItem) {
@@ -30,3 +38,4 @@ selectionEl.onclick = function() {
         document.getElementById("shopImg").src = "img/shopItems/" + currentItem + currentGlaze + ".webp";
     }
 }
+
